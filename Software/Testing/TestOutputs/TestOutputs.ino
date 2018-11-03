ba4264d1e701e073b3d70580574e9348cbff35df
Sam Tusk <tusk_Sam@hotmail.com>
2018-11-03 14:11:40 +0000

    Fixed LED output
    Fixed a hardware bug with the output LEDs:  the current going into the transitior base was not being limited.

---
+++ Software/Testing/TestOutputs/TestOutputs.ino
@@ -0,0 +1,16 @@
+const int OutCyclePin = 7;
+
+
+void setup() {
+  // put your setup code here, to run once:
+  const int OutCyclePin = 7;
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+digitalWrite(OutCyclePin,HIGH);
+delay(1000);
+
+}
